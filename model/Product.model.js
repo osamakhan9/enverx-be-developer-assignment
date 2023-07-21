d0ea6e7b7b73706c88c21d30aea1b2397f7eb43f
mohammad osama <okosama06@gmail.com>
2023-07-21 19:28:25 +0530

    CRUD model is done

---
+++ model/Product.model.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose")
+
+const CrudSchema=mongoose.Schema({
+    images:{
+        type:String,
+        required:true
+    },
+    blog_name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    Price:{
+        type:Number,
+        required:true
+    },
+	category:{
+		type:String,
+		require: true
+	}
+})
+
+const CrudModel=mongoose.model("Crud",CrudSchema)
+
+
+module.exports=CrudModel
